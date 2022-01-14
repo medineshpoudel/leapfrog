9a7e123eb9d0657364947cbc0ebb8af248153ede
medineshpoudel <dineshpoudel58@gmail.com>
2022-01-14 07:16:40 +0545

    level2 fixed

--- Javascript/finalAssignment/src/levels.js
+++ Javascript/finalAssignment/src/levels.js
@@ -1,7 +1,7 @@
 let level = 1;
 let level2 = new Image();
-level2.src = "/images/level2.png";
-let nextSound = new Audio("/sounds/nextLevel.wav");
+level2.src = "images/level2.png";
+let nextSound = new Audio("./sounds/nextLevel.wav");
 let gameintvl = setInterval(() => {
   if (a >= 3300) {
     clearInterval(gameintvl);
