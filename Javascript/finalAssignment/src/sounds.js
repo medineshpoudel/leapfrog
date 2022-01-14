b01ffde7f5bdd19248db21d3e7079e3d3a64034f
medineshpoudel <dineshpoudel58@gmail.com>
2022-01-14 06:49:13 +0545

    sounds edited

--- Javascript/finalAssignment/src/sounds.js
+++ Javascript/finalAssignment/src/sounds.js
@@ -1,6 +1,6 @@
-let enemyHit = new Audio("sounds/enemy-hit.mp3");
-let bulletAudio = new Audio("sounds/bullet.mp3");
-let deathAudio = new Audio("sounds/death.mp3");
-let enemyBullet = new Audio("sounds/enemy-gun.mp3");
-let titleSound = new Audio("sounds/title.mp3");
+let enemyHit = new Audio("./sounds/enemy-hit.mp3");
+let bulletAudio = new Audio("./sounds/bullet.mp3");
+let deathAudio = new Audio("./sounds/death.mp3");
+let enemyBullet = new Audio("./sounds/enemy-gun.mp3");
+let titleSound = new Audio("./sounds/title.mp3");
 // titleSound.play();
